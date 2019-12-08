3e418808709fa6cca02458ee6f995a19bd5546d8
Olga Ioffe <yoffic@yahoo.com>
2019-12-08 17:02:48 +0300

    add args and input option format gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,4 +5,7 @@ const program = require('commander');
 program
   .version('0.0.1', '-V, --version', 'output the current version')
   .description('Compares two configuration files and shows a difference.')
-  .parse(process.argv);
+  .option('-f, --format [type]', 'Output format')
+  .arguments('<firstConfig> <secondConfig>');
+
+program.parse(process.argv);
