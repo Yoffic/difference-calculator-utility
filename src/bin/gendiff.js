5f56bfc10f9e7d0aee8047f27a5634b5340448b0
Olga Ioffe <yoffic@yahoo.com>
2019-12-09 17:04:12 +0300

    feature compare files gendiff.js

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
-
-const program = require('commander');
+import program from 'commander';
+import genDiff from '..';
 
 program
   .version('0.0.1', '-V, --version', 'output the current version')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'Output format')
-  .arguments('<firstConfig> <secondConfig>');
+  .arguments('<firstConfig> <secondConfig>')
+  .action((firstConfig, secondConfig) => console.log(genDiff(firstConfig, secondConfig)));
 
 program.parse(process.argv);
