3ed8cb43f914f0bdc9a52cbb24b3feb977a440e7
Olga Ioffe <yoffic@yahoo.com>
2019-12-11 20:35:55 +0300

    add test ini files

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,8 +2,18 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src';
 
-test('genDiff', () => {
-  const result = fs.readFileSync(path.join(__dirname, '__fixtures__/result1'), 'utf-8');
-  expect(genDiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json')).toEqual(result);
-  expect(genDiff('__tests__/__fixtures__/before.yml', '__tests__/__fixtures__/after.yml')).toEqual(result);
+const result = fs.readFileSync(path.join(__dirname, '__fixtures__/result1'), 'utf-8');
+const mainPath = '__tests__/__fixtures__/';
+const makePath = (curPath, file) => path.join(path.join(mainPath, curPath), file);
+const jsons = 'simpleJson/';
+const yamls = 'simpleYaml/';
+const inis = 'simpleIni/';
+const testFiles = [
+  [makePath(jsons, 'before.json'), makePath(jsons, 'after.json')],
+  [makePath(yamls, 'before.yml'), makePath(yamls, 'after.yml')],
+  [makePath(inis, 'before.ini'), makePath(inis, 'after.ini')],
+];
+
+test.each(testFiles)('differences', (before, after) => {
+  expect(genDiff(before, after)).toEqual(result);
 });
