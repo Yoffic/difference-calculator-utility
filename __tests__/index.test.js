bb48273dfdd85de751742c2bc7e01de10fb827be
Olga Ioffe <yoffic@yahoo.com>
2019-12-11 17:43:33 +0300

    add test for comparing yaml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,4 +5,5 @@ import genDiff from '../src';
 test('genDiff', () => {
   const result = fs.readFileSync(path.join(__dirname, '__fixtures__/result1'), 'utf-8');
   expect(genDiff('__tests__/__fixtures__/before.json', '__tests__/__fixtures__/after.json')).toEqual(result);
+  expect(genDiff('__tests__/__fixtures__/before.yml', '__tests__/__fixtures__/after.yml')).toEqual(result);
 });
