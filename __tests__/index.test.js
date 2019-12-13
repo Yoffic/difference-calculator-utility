db49019d6aa1c240de26a67b5dfad4a1eb0beb21
Olga Ioffe <yoffic@yahoo.com>
2019-12-13 13:39:52 +0300

    update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,5 @@
 import fs from 'fs';
 import path from 'path';
-import { chunk } from 'lodash';
-import sortFiles from '../src/utils';
 import genDiff from '../src';
 
 const resultSimple = fs.readFileSync(path.join(__dirname, '__fixtures__/resultSimple'), 'utf-8');
@@ -10,13 +8,16 @@ const resultComplex = fs.readFileSync(path.join(__dirname, '__fixtures__/resultC
 const pathToSimple = '__tests__/__fixtures__/simple/';
 const pathToComplex = '__tests__/__fixtures__/complex/';
 
-const makePath = (curPath, file) => path.join(curPath, file);
-const getFiles = (dir) => fs.readdirSync(path.join(process.cwd(), dir), 'utf-8');
+const extNames = ['.json', '.yml', '.ini'];
 
-const testSimpleFiles = chunk(sortFiles(getFiles(pathToSimple))
-  .map((file) => makePath(pathToSimple, file)), 2);
-const testComplexFiles = chunk(sortFiles(getFiles(pathToComplex))
-  .map((file) => makePath(pathToComplex, file)), 2);
+const makePath = (curPath, file) => path.join(curPath, file);
+
+const testSimpleFiles = extNames.map((extName) => (
+  [makePath(pathToSimple, `before${extName}`), makePath(pathToSimple, `after${extName}`)]
+));
+const testComplexFiles = extNames.map((extName) => (
+  [makePath(pathToComplex, `before${extName}`), makePath(pathToComplex, `after${extName}`)]
+));
 
 test.each(testSimpleFiles)('differences between simple', (before, after) => {
   expect(genDiff(before, after)).toEqual(resultSimple);
