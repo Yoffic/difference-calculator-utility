bb4f4c604fee963bd6206a47dadc850d130a14a6
Olga Ioffe <yoffic@yahoo.com>
2019-12-15 19:04:05 +0300

    refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,26 +2,16 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src';
 
-const resultSimple = fs.readFileSync(path.join(__dirname, '__fixtures__/resultSimple'), 'utf-8');
-const resultComplex = fs.readFileSync(path.join(__dirname, '__fixtures__/resultComplex'), 'utf-8');
-
-const pathToSimple = '__tests__/__fixtures__/simple/';
-const pathToComplex = '__tests__/__fixtures__/complex/';
+const result = fs.readFileSync(path.join(__dirname, '__fixtures__/result'), 'utf-8');
+const pathToFiles = '__tests__/__fixtures__/files/';
 
 const extNames = ['.json', '.yml', '.ini'];
 
 const makePath = (curPath, file) => path.join(curPath, file);
 
-const testSimpleFiles = extNames.map((extName) => (
-  [makePath(pathToSimple, `before${extName}`), makePath(pathToSimple, `after${extName}`)]
+const testFiles = extNames.map((extName) => (
+  [makePath(pathToFiles, `before${extName}`), makePath(pathToFiles, `after${extName}`)]
 ));
-const testComplexFiles = extNames.map((extName) => (
-  [makePath(pathToComplex, `before${extName}`), makePath(pathToComplex, `after${extName}`)]
-));
-
-test.each(testSimpleFiles)('differences between simple', (before, after) => {
-  expect(genDiff(before, after)).toEqual(resultSimple);
-});
-test.each(testComplexFiles)('differences between complex', (before, after) => {
-  expect(genDiff(before, after)).toEqual(resultComplex);
+test.each(testFiles)('differences between files', (before, after) => {
+  expect(genDiff(before, after)).toEqual(result);
 });
