399311929eb506248a121c57696420260d00ac86
Olga Ioffe <yoffic@yahoo.com>
2019-12-18 12:19:39 +0300

    add tests for plain output format

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,16 +2,26 @@ import fs from 'fs';
 import path from 'path';
 import genDiff from '../src';
 
-const result = fs.readFileSync(path.join(__dirname, '__fixtures__/result'), 'utf-8');
-const pathToFiles = '__tests__/__fixtures__/filesToTest/';
-
+const readFile = (fileName) => fs.readFileSync(path.join(__dirname, `__fixtures__/${fileName}`), 'utf-8');
 const extNames = ['.json', '.yml', '.ini'];
-
+const filesDir = '__tests__/__fixtures__/filesToTest/';
 const makePath = (curPath, file) => path.join(curPath, file);
 
-const testFiles = extNames.map((extName) => (
-  [makePath(pathToFiles, `before${extName}`), makePath(pathToFiles, `after${extName}`)]
+const filesPaths = extNames.map((extName) => (
+  [makePath(filesDir, `before${extName}`), makePath(filesDir, `after${extName}`)]
 ));
-test.each(testFiles)('differences between files', (before, after) => {
-  expect(genDiff(before, after)).toEqual(result);
+
+const formats = ['complex', 'plain'];
+const testArgs = formats
+  .map((format) => [...filesPaths.map((file) => [...file, format])])
+  .flat();
+
+const expected = [readFile('expectedComplex'), readFile('expectedPlain')];
+
+test.each(testArgs)('differences between files', (before, after, format) => {
+  const [complex, plain] = expected;
+  if (format === 'plain') {
+    expect(genDiff(before, after, format)).toEqual(plain);
+  }
+  expect(genDiff(before, after)).toEqual(complex);
 });
