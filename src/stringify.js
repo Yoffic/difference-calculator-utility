f98864dc3a6f2d6ebbf28d755a32c1c5ab75f196
Olga Ioffe <yoffic@yahoo.com>
2019-12-18 16:53:10 +0300

    refactor stringify.js

--- src/stringify.js
+++ src/stringify.js
@@ -1,21 +1,16 @@
-import { isObject } from 'lodash';
-
 export const getSpaces = (val) => ' '.repeat(val * 2);
 
-const stringifyNested = (value, level) => {
-  const newValues = Object.entries(value)
-    .map((entry) => {
-      const [key, curValue] = entry;
-      return isObject(curValue)
+const stringifyNested = (value, level) => (
+  Object.entries(value)
+    .map(([key, curValue]) => (
+      curValue instanceof Object
         ? `${getSpaces(level + 1)}${stringifyNested(curValue, level + 2)}`
-        : `${getSpaces(level + 1)}${key}: ${curValue}`;
-    }).join('\n');
-  return newValues;
-};
+        : `${getSpaces(level + 1)}${key}: ${curValue}`
+    )).join('\n')
+);
 
-export const stringify = (key, value, level) => {
-  if (isObject(value)) {
-    return `${getSpaces(level)}${key}: {\n${stringifyNested(value, level + 2)}\n${getSpaces(level + 1)}}`;
-  }
-  return `${getSpaces(level)}${key}: ${value}`;
-};
+export const stringify = (key, value, level) => (
+  value instanceof Object
+    ? `${getSpaces(level)}${key}: {\n${stringifyNested(value, level + 2)}\n${getSpaces(level + 1)}}`
+    : `${getSpaces(level)}${key}: ${value}`
+);
