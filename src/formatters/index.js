ac66b0e4ebb40b180848f8f8537b5d60420566ce
Olga Ioffe <yoffic@yahoo.com>
2019-12-18 16:15:41 +0300

    add json output format functionality

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,10 +1,9 @@
 import getComplex from './complex';
 import getPlain from './plain';
+import getJson from './json';
 
 export default (data, format) => {
-  if (format === 'plain') {
-    return getPlain(data);
-  }
-
+  if (format === 'plain') return getPlain(data);
+  if (format === 'json') return getJson(data);
   return getComplex(data);
 };
