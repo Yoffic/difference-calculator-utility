2c27b476e2cd75b6a756c97e0407fdc9ecf63db4
Olga Ioffe <yoffic@yahoo.com>
2019-12-18 16:29:43 +0300

    refactor buildDiff.js

--- src/buildDiff.js
+++ src/buildDiff.js
@@ -12,26 +12,14 @@ const buildDiff = (data1, data2) => {
     }
 
     if (data1[key] instanceof Object && data2[key] instanceof Object) {
-      return {
-        key,
-        type: 'unchanged',
-        children: buildDiff(data1[key], data2[key]),
-      };
+      return { key, type: 'unchanged', children: buildDiff(data1[key], data2[key]) };
     }
 
     if (data1[key] !== data2[key]) {
-      return {
-        key,
-        type: 'updated',
-        value: [data1[key], data2[key]],
-      };
+      return { key, type: 'updated', value: [data1[key], data2[key]] };
     }
 
-    return {
-      key,
-      type: 'unchanged',
-      value: data2[key],
-    };
+    return { key, type: 'unchanged', value: data2[key] };
   });
   return diff;
 };
