892cd072af6f30a45b0c4097d5f3adfd4309a125
Olga Ioffe <yoffic@yahoo.com>
2019-12-18 16:14:57 +0300

    add tests for json output

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -11,17 +11,20 @@ const filesPaths = extNames.map((extName) => (
   [makePath(filesDir, `before${extName}`), makePath(filesDir, `after${extName}`)]
 ));
 
-const formats = ['complex', 'plain'];
+const formats = ['complex', 'plain', 'json'];
 const testArgs = formats
   .map((format) => [...filesPaths.map((file) => [...file, format])])
   .flat();
 
-const expected = [readFile('expectedComplex'), readFile('expectedPlain')];
+const expected = [readFile('expectedComplex'), readFile('expectedPlain'), readFile('expectedJson')];
 
 test.each(testArgs)('differences between files', (before, after, format) => {
-  const [complex, plain] = expected;
+  const [complex, plain, json] = expected;
   if (format === 'plain') {
     expect(genDiff(before, after, format)).toEqual(plain);
+  }
+  if (format === 'json') {
+    expect(genDiff(before, after, format)).toEqual(json);
   }
   expect(genDiff(before, after)).toEqual(complex);
 });
