7c140f9558baa989c9233dbd82b5caf80f5d3629
Olga Ioffe <yoffic@yahoo.com>
2019-12-24 21:31:37 +0300

    refactor tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,23 +5,14 @@ const readFile = (filepath) => fs.readFileSync(filepath, 'utf-8');
 const getPath = (filename) => `__tests__/__fixtures__/${filename}`;
 
 const filetypes = ['ini', 'json', 'yml'];
-
 const getArgs = (format) => filetypes.map((filetype) => (
   [getPath(`before.${filetype}`), getPath(`after.${filetype}`), format]
 ));
 
-describe('generate difference', () => {
-  const testCallback = (before, after, format) => {
-    const output = readFile(getPath(format));
-    expect(genDiff(before, after, format)).toEqual(output);
-  };
+const formats = ['complex', 'plain', 'json'];
+const testArgs = formats.map((format) => getArgs(format)).flat();
 
-  const testArgs1 = getArgs('complex');
-  test.each(testArgs1)('complex output %p, %p', testCallback);
-
-  const testArgs2 = getArgs('plain');
-  test.each(testArgs2)('plain output %p, %p', testCallback);
-
-  const testArgs3 = getArgs('json');
-  test.each(testArgs3)('json output %p, %p', testCallback);
+test.each(testArgs)('compare %s, %s to have %p output format', (before, after, format) => {
+  const output = readFile(getPath(format));
+  expect(genDiff(before, after, format)).toEqual(output);
 });
