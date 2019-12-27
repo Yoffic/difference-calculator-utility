b043008b781b0e60dc704a990b4957a0580d3ad4
Olga Ioffe <yoffic@yahoo.com>
2019-12-27 18:36:38 +0300

    refactor index.js add read file to genDiff

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
+import { getType, getContent } from './utils';
 import parse from './parsers';
 import buildDiff from './buildDiff';
 import getOutput from './formatters';
 
 const genDiff = (path1, path2, format = 'complex') => {
-  const data1 = parse(path1);
-  const data2 = parse(path2);
+  const filetype1 = getType(path1);
+  const filetype2 = getType(path2);
+
+  const content1 = getContent(path1);
+  const content2 = getContent(path2);
+
+  const data1 = parse(filetype1, content1);
+  const data2 = parse(filetype2, content2);
+
   const difference = buildDiff(data1, data2);
 
   return getOutput(difference, format);
