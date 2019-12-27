c20786f5011a88c779ecb8136637fcd8731956d6
Olga Ioffe <yoffic@yahoo.com>
2019-12-27 18:33:46 +0300

    refactor parsers.js

--- src/parsers.js
+++ src/parsers.js
@@ -1,26 +1,10 @@
-import fs from 'fs';
-import path from 'path';
 import yaml from 'js-yaml';
 import ini from 'ini';
 
 const parsers = {
-  '.json': (content) => JSON.parse(content),
-  '.yml': (content) => yaml.safeLoad(content),
-  '.ini': (content) => ini.parse(content),
+  '.json': JSON.parse,
+  '.yml': yaml.safeLoad,
+  '.ini': ini.parse,
 };
 
-const getExt = (filepath) => path.extname(filepath);
-const getParser = (type) => parsers[type];
-
-const getWorkdir = () => process.cwd();
-const getAbspath = (dirpath, filepath) => path.join(dirpath, filepath);
-const getContent = (filepath) => fs.readFileSync(filepath, 'utf-8');
-
-export default (filepath) => {
-  const parse = getParser(getExt(filepath));
-
-  const absFilepath = getAbspath(getWorkdir(), filepath);
-  const content = getContent(absFilepath);
-
-  return parse(content);
-};
+export default (type, content) => parsers[type](content);
