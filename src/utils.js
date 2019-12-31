cee325b534515cc377e629c0c5aa37fdf292f69a
Olga Ioffe <yoffic@yahoo.com>
2019-12-31 12:15:20 +0300

    refactor parsing types

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
 
-export const getType = (filepath) => path.extname(filepath);
+export const getType = (filepath) => path.extname(filepath).slice(1);
 
 export const getContent = (filepath) => fs.readFileSync(path.resolve(process.cwd(), filepath), 'utf-8');
